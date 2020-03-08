fb3f82c2a3d495ef83b3c132bc1f6ee8226a3473
VTC <vtchris@hotmail.com>
2020-03-08 18:13:20 -0400

    Initial styling

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,4 +1,48 @@
+function load_game(){
+    let mainDiv = document.getElementById('mainDiv');
+    let newCard = document.createElement('div');
+    newCard.classList.add('card');
+    //newCard.style = 'width:18rem;'
+    let newCardHeader = document.createElement('div');
+    newCardHeader.classList.add('card-header');
+    newCardHeader.innerText = 'Select number of players:';
+    let newCardBody =document.createElement('div');
+    newCardBody.classList.add('card-body');
+    newCardBody.appendChild(createSelectList(['1 Player','2 Players']));
+    let newCardFooter = document.createElement('footer');
+    newCardFooter.classList.add('card-footer');
+    let newBtn = document.createElement('button');
+    newBtn.classList.add('btn','btn-primary');
+    newBtn.innerText = 'NEXT';
+    newCardFooter.appendChild(newBtn);
 
+
+    newCard.appendChild(newCardHeader);
+    newCard.appendChild(newCardBody);
+    newCard.appendChild(newCardFooter);
+
+    mainDiv.appendChild(newCard);
+    
+
+
+
+}
+
+function createSelectList(options){
+    let select = document.createElement('select');
+    
+    for(let i = 0;i<options.length;i++){
+        let newOption = document.createElement('option');
+        newOption.value = i;
+        newOption.innerText = options[i];
+        select.appendChild(newOption)
+    }
+    
+    return select;
+
+}
+
+load_game();
 
 let puzzles = []
 puzzles.push(new Puzzle("movie","the perks of being a wallflower"))
